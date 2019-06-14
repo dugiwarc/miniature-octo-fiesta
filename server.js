f0d2be255947ac062a02238d1a536a4cdbbc5f26
dugi <contact@tusk.world>
2019-06-14 13:00:04 +0200

    add server db proxy react

---
+++ server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+var http = require("http");
+var server = http.createServer(app);
+const mongoose = require("mongoose");
+var io = require("socket.io")(http);
+
+// console.log(io);
+connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log("MongoDB connected");
+    io.on("connection", async socket => {
+      try {
+        console.log("user connected");
+      } catch (error) {
+        console.log("not connected");
+      }
+    });
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+const path = require("path");
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+server.listen(process.env.PORT, () =>
+  console.log(`Server started on port ${process.env.PORT}`)
+);
